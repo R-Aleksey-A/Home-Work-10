6dcf3ae93dae8fdc98fc55f5bd11fdfb7438aa4b
huawei <romanec.aleksey@gmail.com>
2024-12-24 00:37:10 +0400

    Master

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work 10");
-        System.out.println();
-        System.out.println("Задание-1");
-        String firstName ="Ivan";
-        String middleName=" Ivanovich";
-        String lastName="Ivanov ";
-        String fullName= lastName + firstName + middleName;
-        System.out.println("Ф. И. О. сотрудника — "+fullName);
+
 
 
     }
