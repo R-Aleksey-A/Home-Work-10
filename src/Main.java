19dfe46b40a1734a40a700c7ef429aa2c27cee5d
huawei <romanec.aleksey@gmail.com>
2024-12-24 00:45:57 +0400

    HW10.2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work 10");
+
         System.out.println();
         System.out.println("Задание-1");
-        String firstName ="Ivan";
-        String middleName=" Ivanovich";
-        String lastName="Ivanov ";
-        String fullName= lastName + firstName + middleName;
-        System.out.println("Ф. И. О. сотрудника — "+fullName);
+        String firstName = "Ivan";
+        String middleName = " Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println();
+        System.out.println("Задание-2");
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName1);
+
     }
 }
